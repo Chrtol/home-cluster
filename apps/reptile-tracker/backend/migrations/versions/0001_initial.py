b5f6216e982163b0a70a4bfaba7d0ec332ea7d72
Christian <chrtol95@gmail.com>
2025-10-12 15:40:01 +0200

    fix(reptile-tracker): update revision numbers for revision files

--- apps/reptile-tracker/backend/migrations/versions/0001_initial.py
+++ apps/reptile-tracker/backend/migrations/versions/0001_initial.py
@@ -1,6 +1,6 @@
 """initial schema
 
-Revision ID: 0001_initial
+Revision ID: 0001
 Revises:
 Create Date: 2025-10-12 00:00:00.000000
 
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = '0001_initial'
+revision = '0001'
 down_revision = None
 branch_labels = None
 depends_on = None
